4452449ec5c2a491fb50fb8cf204c5e4f24a8b0c
DenisKantic <denis.kantic18@gmail.com>
2020-03-18 20:35:13 +0100

    created eventlist... for filterInput and function

---
+++ app.js
@@ -0,0 +1,13 @@
+let filterInput = document.getElementById('filterInput');
+
+filterInput.addEventListener('keyup', filterNames);
+
+
+function filterNames(){
+
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+
+    // get names ul
+
+}
