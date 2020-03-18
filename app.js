283159acf5bd25ec52b3fc6faa8b91e06f66b661
DenisKantic <denis.kantic18@gmail.com>
2020-03-18 20:38:38 +0100

    fully created function in javascript

--- app.js
+++ app.js
@@ -10,4 +10,26 @@ function filterNames(){
 
     // get names ul
 
+    let ul = document.getElementById('names');
+
+    // get li from names 
+
+    let li = ul.querySelectorAll('li.collection-item');
+
+    // loop through collection-item
+
+    for ( let i=0; i<li.length; i++){
+        let a=li[i].getElementsByTagName('a')[0];
+
+
+        // if matched
+        if (a.innerHTML.toUpperCase().indexOf(filterValue )> -1){
+
+            li[i].style.display= '';
+        } else {
+
+            li[i].style.display = 'none';
+        }
+    }
+
 }
